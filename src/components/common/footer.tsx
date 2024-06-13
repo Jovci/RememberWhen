f40dba85ca9ee65e5e1452d29872821997b3a550
Jovci <70718127+Jovci@users.noreply.github.com>
2024-06-12 22:19:41 -0700

    Footer

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -7,39 +7,26 @@ export function Footer() {
     <footer className="flex h-12 items-center justify-center w-full border-t">
       <div className="w-full max-w-[1280px] md:px-8 px-4 flex place-content-center">
         <div className="gap-x-11 md:flex flex-1 hidden">
-          <Link
-            href="/"
-            className="pointer flex items-center"
-          >
-            <img src="/logo.svg" className="mr-3" />
-            <Typography className="!text-white !text-base font-medium ">
+          <Link href="/" className="pointer flex items-center">
+            <img src="/logo.svg" className="mr-3" alt="Logo" />
+            <Typography className="!text-white !text-base font-medium">
               RememberWhen
             </Typography>
           </Link>
         </div>
-        <div className="flex max-w-fit items-center gap-x-4">
-          <Link
-            href="Boilerplate"
-            target="_blank"
-            className="pointer block w-fit flex-1"
-          >
-            <Typography variant="p" className="w-max">
-              Not a social media app
+        <div className="flex flex-nowrap items-center gap-x-4">
+          <Link href="Boilerplate" target="_blank" className="pointer block w-fit flex-1">
+            <Typography variant="p" className="whitespace-nowrap w-max">
+              Not A Social Media App
             </Typography>
           </Link>
-          <Link
-            href="/terms-of-service"
-            className="pointer block w-fit flex-1"
-          >
-            <Typography variant="p" className="w-max">
+          <Link href="/terms-of-service" className="pointer block w-fit flex-1">
+            <Typography variant="p" className="whitespace-nowrap w-max">
               Terms of service
             </Typography>
           </Link>
-          <Link
-            href="/privacy-policy"
-            className="pointer block w-fit"
-          >
-            <Typography variant="p">
+          <Link href="/privacy-policy" className="pointer block w-fit flex-1">
+            <Typography variant="p" className="whitespace-nowrap w-max">
               Privacy Policy
             </Typography>
           </Link>
