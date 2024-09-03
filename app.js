a02b66b2971dd3a7aeb28d536663993d52dbbeec
Jovci <70718127+Jovci@users.noreply.github.com>
2024-09-03 16:25:47 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.get('/index', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Optional: Redirect from /index.html to /index
+app.get('/index.html', (req, res) => {
+  res.redirect('/index');
 });
 
 const PORT = process.env.PORT || 3000;
